3d1727708212af899841ce9d7508bf513e8bdd42
Daria Dobrolinski <dsdobrolinski@gmail.com>
2025-06-30 21:56:03 -0400

    Add main functionality for restaurant recommendation system and update .gitignore

---
+++ main.py
@@ -0,0 +1,47 @@
+from data import restaurant_data
+import stdio
+
+head = None
+
+class Node:
+    def __init__(self, cuisine, name, price, distance, address, nextNode = None):
+        self.cuisine = cuisine
+        self.name = name
+        self.price = price
+        self.distance = distance
+        self.address = address
+        self.nextNode = nextNode
+
+for restaurant in restaurant_data:
+    newNode = Node(restaurant[0], restaurant[1], restaurant[2], restaurant[3], restaurant[4])
+    if head is None:
+        head = newNode
+    else:
+        current = head
+        while current.nextNode:
+            current = current.nextNode
+        current.nextNode = newNode
+        
+
+print("What cuisine would you like to eat today?\n")
+targetCuisine = stdio.readString()
+
+def searchCuisine(head, targetCuisine):
+    current = head
+    matches = []
+
+    while current:
+        if current.cuisine == targetCuisine:
+            matches.append(current)
+        current = current.nextNode
+
+    return matches
+    
+results = searchCuisine(head, targetCuisine)
+
+if not results:
+    print(f"\nNo {targetCuisine} spots found!")
+else:
+    for node in results:
+        print(f"\n{node.name} - Price: {node.price}/5, Distance: {node.distance}/5, - {node.address}\n")
+
